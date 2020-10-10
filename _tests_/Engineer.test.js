f2d0d619830868fa73c4afad9e2fd2857bd57737
Josh Carter <joshcarter8400@gmail.com>
2020-10-10 15:57:53 -0400

    added Engineer Tests

---
+++ _tests_/Engineer.test.js
@@ -0,0 +1,22 @@
+const { TestScheduler } = require("jest");
+const Engineer = require("../lib/Engineer.js");
+
+const engineer = new Engineer("Josh", 2, "jc84");
+
+test("creates an engineer", () => {
+  expect(engineer.getRole()).toEqual(expect.any(String));
+});
+
+test("get's name of engineer", () => {
+  expect(engineer.getName()).toEqual("Josh");
+});
+
+test("check to see if ID is equal to 2", () => {
+  expect(engineer.getId()).toEqual(2);
+});
+
+test("check to see if all input is valid", () => {
+  expect(engineer.getId()).toEqual(2);
+  expect(engineer.getName()).toEqual("Josh");
+  expect(engineer.getEmail()).toEqual("jc84");
+});
